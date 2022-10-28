d5db0b9fa2c2567000938e2d70c16ce913f9ee72
j1powder <93947216+j1powder@users.noreply.github.com>
2022-10-27 19:01:20 -0700

    Add files via upload

---
+++ Brittany's new website(yet again)/app.js
@@ -0,0 +1,213 @@
+$(document).ready(function(){
+    $('#latest1-btn').click(function(){
+     $('#latest1').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+ $(document).ready(function(){
+    $('#latest2-btn').click(function(){
+     $('#latest2').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+ $(document).ready(function(){
+    $('#latest3-btn').click(function(){
+     $('#latest3').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+ $(document).ready(function(){
+    $('#latest4-btn').click(function(){
+     $('#latest4').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+ $(document).ready(function(){
+    $('#latest5-btn').click(function(){
+     $('#latest5').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+ $(document).ready(function(){
+    $('#latest6-btn').click(function(){
+     $('#latest6').css("display", "block");
+     $('#top-product-pg-modal').css("display", "block");
+     $('#backdrop').css("display", "block");
+     $('#hide-on-modal').hide();
+    }) ;
+ });
+
+ $(document).ready(function(){
+   $('#halloween1-btn').click(function(){
+    $('#halloween1').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#halloween2-btn').click(function(){
+    $('#halloween2').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#halloween3-btn').click(function(){
+    $('#halloween3').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#halloween4-btn').click(function(){
+    $('#halloween4').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#halloween5-btn').click(function(){
+    $('#halloween5').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#halloween6-btn').click(function(){
+    $('#halloween6').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+
+
+$(document).ready(function(){
+   $('#xmas1-btn').click(function(){
+    $('#xmas1').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#xmas2-btn').click(function(){
+    $('#xmas2').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#xmas3-btn').click(function(){
+    $('#xmas3').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#xmas4-btn').click(function(){
+    $('#xmas4').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#xmas5-btn').click(function(){
+    $('#xmas5').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#xmas6-btn').click(function(){
+    $('#xmas6').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion1-btn').click(function(){
+    $('#anyoccasion1').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion2-btn').click(function(){
+    $('#anyoccasion2').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion3-btn').click(function(){
+    $('#anyoccasion3').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion4-btn').click(function(){
+    $('#anyoccasion4').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion5-btn').click(function(){
+    $('#anyoccasion5').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
+$(document).ready(function(){
+   $('#anyoccasion6-btn').click(function(){
+    $('#anyoccasion6').css("display", "block");
+    $('#top-product-pg-modal').css("display", "block");
+    $('#backdrop').css("display", "block");
+    $('#hide-on-modal').hide();
+   }) ;
+});
+
